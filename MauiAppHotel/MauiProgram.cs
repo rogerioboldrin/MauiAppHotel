470ef451bc33991b0d06c43cd96a40d260846fcd
rogerioboldrin <rogerioboldrin@gmail.com>
2024-11-02 14:05:41 -0300

    Add recursos ao app

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,15 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
+                    fonts.AddFont("Kalam-Light.ttf", "KalamLight");
+                    fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("Poppins-Black.ttf", "PoppinsBlack");
+                    fonts.AddFont("Poppins-Bold.ttf", "PoppinsBold");
+                    fonts.AddFont("Poppins-BoldItalic.ttf", "PoppinsBoldItalic");
+                    fonts.AddFont("Poppins-ExtraBoldItalic.ttf", "PoppinsExtraBoldItalic");
+                    fonts.AddFont("Poppins-Italic.ttf", "PoppinsItalic");
+
                 });
 
 #if DEBUG
